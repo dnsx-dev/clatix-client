1a585dae7f9abf9f5571466b17c682198d91f03c
dnsx-dev <adriandumitrascu11ani@gmail.com>
2026-08-30 08:04:47 +0300

    Refactor ModuleButton to use final fields and update colors

--- src/main/java/com/clatix/gui/ModuleButton.java
+++ src/main/java/com/clatix/gui/ModuleButton.java
@@ -4,12 +4,13 @@ import com.clatix.modules.Module;
 import com.clatix.utils.RenderUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.InputUtil;
 
 import java.awt.*;
 
 public class ModuleButton {
-    private MinecraftClient mc = MinecraftClient.getInstance();
-    private Module module;
+    private final MinecraftClient mc = MinecraftClient.getInstance();
+    private final Module module;
     private int x, y, width, height;
     private boolean hovered = false;
 
@@ -23,23 +24,25 @@ public class ModuleButton {
 
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         hovered = isMouseOver(mouseX, mouseY);
-        Color color = module.isEnabled() ? new Color(0, 150, 255, 200) : 
-                      (hovered ? new Color(60, 60, 60, 255) : new Color(40, 40, 40, 255));
-        
-        RenderUtils.drawRect(context, x, y, x + width, y + height, color.getRGB());
-        RenderUtils.drawBorder(context, x, y, x + width, y + height, 1, new Color(60, 60, 60, 255).getRGB());
-        
+        Color bg = module.isEnabled() ? new Color(0, 212, 255, 60) :
+                (hovered ? new Color(255, 255, 255, 20) : new Color(20, 20, 20, 200));
+        RenderUtils.drawRect(context, x, y, x + width, y + height, bg.getRGB());
+        RenderUtils.drawBorder(context, x, y, x + width, y + height, 1, new Color(40, 40, 40).getRGB());
+
         String display = module.getName();
         if (module.getKey() != 0) {
-            display += " [" + net.minecraft.client.util.InputUtil.getKeyName(module.getKey()) + "]";
+            display += " [" + InputUtil.getKeyName(module.getKey()) + "]";
         }
-        context.drawText(mc.textRenderer, display, x + 4, y + height / 2 - 4, 0xFFFFFF, false);
+        int textColor = module.isEnabled() ? 0x00D4FF : 0xAAAAAA;
+        context.drawText(mc.textRenderer, display, x + 4, y + height / 2 - 4, textColor, false);
     }
 
     public void mouseClicked(double mouseX, double mouseY, int button) {
         if (isMouseOver(mouseX, mouseY)) {
             if (button == 0) {
                 module.toggle();
+            } else if (button == 1) {
+                // TODO: open settings panel
             }
         }
     }
